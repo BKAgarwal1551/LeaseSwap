cce3cf0aa2c3b563a28880a4060881d2b2c775d4
Bhavesh Agarwal <bkagarwal1551@gmail.com>
2026-01-28 13:41:53 +0000

    fix(mobile): wrap app in GestureHandlerRootView for swipe gestures

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,16 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { RootNavigator } from './src/navigation/RootNavigator';
 import { useTheme } from './src/theme/useTheme';
 
 export default function App() {
   const { mode } = useTheme();
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <RootNavigator />
       <StatusBar style={mode === 'dark' ? 'light' : 'dark'} />
-    </>
+    </GestureHandlerRootView>
   );
 }
